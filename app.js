15a828b1cdf9f676581f622444b32a4de768377e
allstons <jagschwartz@aol.com>
2015-04-23 11:39:13 -0400

    more paths

--- app.js
+++ app.js
@@ -11,23 +11,23 @@
   .config(function($routeProvider){
     $routeProvider
     .when('/', {
-      templateUrl: 'fronting/views/splash.html',
+      templateUrl: 'views/splash.html',
       controller: 'MainController as mainCtrl'
     })
     .when('/about', {
-      templateUrl: 'fronting/views/about.html',
+      templateUrl: 'views/about.html',
       controller: 'MainController as mainCtrl'
     })
     .when('/splash', {
-      templateUrl: 'fronting/views/splash.html',
+      templateUrl: 'views/splash.html',
       controller: 'MainController as mainCtrl'
     })
     .when('/not-found', {
-      templateUrl: 'fronting/views/404.html',
+      templateUrl: 'views/404.html',
       controller: 'MainController as mainCtrl'
     })
     .when('/favorites', {
-      templateUrl: 'fronting/front/front.favorites.html',
+      templateUrl: 'front/front.favorites.html',
       controller: 'FavoriteController as favorite'
     })
     .otherwise({
