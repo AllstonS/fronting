6ba93da15e724ab6696027e77ef429d8d889e1fc
allstons <jagschwartz@aol.com>
2015-04-23 11:12:49 -0400

    correcting paths

---
+++ gh-pages/fronting/app.js
@@ -0,0 +1,40 @@
+(function () {
+  "use strict";
+  angular.module('frontin', [
+  'ngRoute',
+  'ngAnimate',
+  'uiGmapgoogle-maps',
+  'admin',
+  'login',
+  'frontmain'
+  ])
+  .config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+      templateUrl: '/views/splash.html',
+      controller: 'MainController as mainCtrl'
+    })
+    .when('/about', {
+      templateUrl: '/views/about.html',
+      controller: 'MainController as mainCtrl'
+    })
+    .when('/splash', {
+      templateUrl: 'views/splash.html',
+      controller: 'MainController as mainCtrl'
+    })
+    .when('/not-found', {
+      templateUrl: 'views/404.html',
+      controller: 'MainController as mainCtrl'
+    })
+    .when('/favorites', {
+      templateUrl: '/front/front.favorites.html',
+      controller: 'FavoriteController as favorite'
+    })
+    .otherwise({
+      redirectTo: '/not-found'
+    })
+  })
+  .constant('_', _)
+
+
+})();
